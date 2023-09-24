fb78fc169c289cf298ccb791a32b4787d9ff3dc3
Porter Squires <PyerterSquires@gmail.com>
2023-09-23 20:35:14 -0400

    Added run-idle animation flipping

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] protected Rigidbody2D rb;
 
     [SerializeField] protected float speed = 10;
+    [SerializeField] protected bool flipped = false;
 
     private void Awake()
     {
@@ -22,8 +20,39 @@ public class PlayerController : MonoBehaviour
 
     public void Move(InputAction.CallbackContext input)
     {
-        Vector2 direction = input.ReadValue<Vector2>();
-        rb.velocity = direction * speed;
+        if (input.canceled)
+            rb.velocity = Vector3.zero;
+        else
+        {
+            Vector2 direction = input.ReadValue<Vector2>();
+            rb.velocity = direction * speed;
+        }
+        anim.SetFloat("Speed", rb.velocity.magnitude);
+        CheckFlip();
+    }
+
+    public void CheckFlip()
+    {
+        bool shouldFlip = false;
+        if (!flipped && rb.velocity.x < 0)
+        {
+            shouldFlip = true;
+        }
+        else if (flipped && rb.velocity.x > 0)
+        {
+            shouldFlip = true;
+        }
+
+        if (shouldFlip)
+            Flip();
+    }
+
+    protected void Flip()
+    {
+        flipped = !flipped;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 
     private void FixedUpdate()
